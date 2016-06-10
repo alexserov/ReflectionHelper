d6c1c9bfc89b8b96a4ce769b02714d3d65f0a01a
Serov.Alexey <Serov.Alexey@SEROV-NB-W10>
2016-06-10 17:50:44 +0300

    agawrg

--- ReflectionHelper/Helpers/Attributes.cs
+++ ReflectionHelper/Helpers/Attributes.cs
@@ -1,8 +1,31 @@
 using System;
+using System.Reflection;
 
 namespace DevExpress.Xpf.Core.Internal {
     public static class ReflectionHelperAttributes {
         [AttributeUsage(AttributeTargets.Property)]
         public class FieldAccessorAttribute : Attribute {}
+
+        [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+        public class BindingFlagsAttribute : Attribute {
+            private readonly BindingFlags flags;
+
+            public BindingFlagsAttribute(BindingFlags flags) {
+                this.flags = flags;
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+        public class NameAttribute : Attribute {
+            private readonly string name;
+
+            public NameAttribute(string name) {
+                this.name = name;
+            }
+        }
+        [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property)]
+        public class WrapResultAttribute : Attribute {
+            
+        }
     }
 }
